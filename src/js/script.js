d5e4b10736ebaf91c0cac6f8e3768d96ca7c50e2
htjuung <htjuung@gmail.com>
2024-05-27 20:33:17 +0800

    adjust rwd

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+$('.btn').click(function() {
+    $('.btn').toggleClass('active');
+    $('.hamburger-icon').toggleClass('close');
+    $('.closure').toggleClass('close');
+    $('ul').toggleClass('close');
+  })
